15adb4a85556b56a78a1b8634d59db770e8db4bb
Paulo Carvalho <pauloej.carvalho@gmail.com>
2022-05-16 15:13:59 -0300

    feat(kafka): including kafka configuration on Classroom Service

--- classroom/src/app.module.ts
+++ classroom/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { HttpModule } from './http/http.module';
+import { MessasingModule } from './messasing/messasing.module';
 @Module({
-  imports: [DatabaseModule, HttpModule],
+  imports: [DatabaseModule, HttpModule, MessasingModule],
   controllers: [],
   providers: [],
 })
--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -5,16 +5,19 @@ import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
 
 import { DatabaseModule } from '../database/database.module';
+import { MessagingModule } from '../messaging/messaging.module';
 import { ProductsResolver } from './graphql/resolvers/products.resolver';
 import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
 import { ProductsService } from '../services/products.service';
 import { PurchasesService } from '../services/purchases.service';
 import { CustomersService } from '../services/customers.service';
 import { CustomersResolver } from './graphql/resolvers/customers.resolver';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
+    MessagingModule,
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
