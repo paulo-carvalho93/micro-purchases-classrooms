52affcf988d57d42fb56b39ebb79fc354b1db0b6
Paulo Carvalho <pauloej.carvalho@gmail.com>
2022-05-16 15:35:26 -0300

    feat(classroom): including integration purchases with kafka

--- classroom/src/services/enrollments.service.ts
+++ classroom/src/services/enrollments.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma/prisma.service';
 
-interface getByCourseAndStudentIdParams {
+interface CreateEnrollmentParams {
+  courseId: string;
+  studentId: string;
+}
+interface GetByCourseAndStudentIdParams {
   courseId: string;
   studentId: string;
 }
@@ -12,7 +16,7 @@ export class EnrollmentsService {
   getByCourseAndStudentId({
     courseId,
     studentId,
-  }: getByCourseAndStudentIdParams) {
+  }: GetByCourseAndStudentIdParams) {
     return this.prisma.enrollment.findFirst({
       where: {
         courseId,
@@ -41,6 +45,15 @@ export class EnrollmentsService {
       },
       orderBy: {
         createdAt: 'desc',
+      },
+    });
+  }
+
+  createEnrollment({ courseId, studentId }: CreateEnrollmentParams) {
+    return this.prisma.enrollment.create({
+      data: {
+        courseId,
+        studentId,
       },
     });
   }
