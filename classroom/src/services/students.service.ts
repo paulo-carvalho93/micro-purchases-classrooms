5fb471f49c9c8eff4fa7592b95f370bf298e7410
Paulo Carvalho <pauloej.carvalho@gmail.com>
2022-05-16 11:25:00 -0300

    feat(classroom): including resolvers and services functions

--- classroom/src/services/students.service.ts
+++ classroom/src/services/students.service.ts
@@ -4,4 +4,16 @@ import { PrismaService } from '../database/prisma/prisma.service';
 @Injectable()
 export class StudentsService {
   constructor(private prisma: PrismaService) {}
+
+  listAllStudents() {
+    return this.prisma.student.findMany();
+  }
+
+  getStudentById(id: string) {
+    return this.prisma.student.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
 }
